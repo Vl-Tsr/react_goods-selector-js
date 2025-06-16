998ca8d5befd069de627c7620685cf17a207577b
Vladyslav Tsarenko <vaaaltaaasaaar@gmail.com>
2025-06-16 15:01:36 -0500

    add solution

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,9 @@ export const App = () => {
               type="button"
               className="delete ml-3"
               onClick={() => setGood('')}
-            />
+            >
+              {' '}
+            </button>
           </>
         ) : (
           'No goods selected'
